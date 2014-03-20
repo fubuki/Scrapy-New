eedb69a384a86c432bb8843e440c656f34fa16d1
fubuki <fubuki@users.noreply.github.com>
2014-03-20 23:34:03 +0800

    增加抓取cnet的spider 參考自 http://orangain.hatenablog.com/entry/scrapy

--- tutorial/spiders/yahoo.py
+++ tutorial/spiders/yahoo.py
@@ -1,18 +1,34 @@
 from scrapy.spider import Spider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from tutorial.items import YahooItem
 
-class YahooSpider(Spider):
+class YahooSpider(CrawlSpider):
     name = "yahoo"
     allowed_domains = ["tw.news.yahoo.com"]
     start_urls = [
-        "http://tw.news.yahoo.com/%E8%97%8F%E6%B5%B7%E6%B4%9B%E5%9B%A0%E9%97%96%E9%97%9C-%E7%98%B8%E8%85%BF%E7%94%B7%E5%88%A4%E5%88%91%E5%AE%9A%E8%AE%9E-040807265.html"
+        "http://tw.news.yahoo.com/"
     ]
-
+    rules = (
+        # specific for golem.de -- remove for other sites
+        #Rule(SgmlLinkExtractor(allow=('news\/',)), callback='parse_page', follow=True),
+        Rule(SgmlLinkExtractor(allow=('.html')), callback='parse', follow=True),
+    )
     def parse(self, response):
+        self.log('Hi, this is an item page! %s' % response.url)
+
+
+        item = YahooItem()
+        #item['id'] = sel.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+        item['title'] = sel.xpath('//td[@id="item_name"]/text()').extract()
+        item['body'] = sel.xpath('//td[@id="item_description"]/text()').extract()
+
+
+        #'''
         item = YahooItem()
         sel = Selector(response)
-                    
+                
         title = sel.xpath('//h1[@class="headline"]/text()').extract()
         body = sel.xpath('//div[@class="bd"]/text()').extract()
         if 0 < len(title):
@@ -22,3 +38,4 @@ class YahooSpider(Spider):
             item['title'] = ''
             item['body'] = ''
         yield item
+        #'''
