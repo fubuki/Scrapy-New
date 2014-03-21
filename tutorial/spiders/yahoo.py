936f81b873c7c157d58b4bb5a7ad3d2e2e992eed
fubuki <fubuki@users.noreply.github.com>
2014-03-21 23:30:54 +0800

    修正SgmlLinkExtractor沒有正確抓取新聞連結的問題。

--- tutorial/spiders/yahoo.py
+++ tutorial/spiders/yahoo.py
@@ -1,31 +1,25 @@
-from scrapy.spider import Spider
+
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
+
 from tutorial.items import YahooItem
 
 class YahooSpider(CrawlSpider):
     name = "yahoo"
     allowed_domains = ["tw.news.yahoo.com"]
     start_urls = [
-        "http://tw.news.yahoo.com/"
+        "http://tw.news.yahoo.com"
     ]
     rules = (
-        # specific for golem.de -- remove for other sites
-        #Rule(SgmlLinkExtractor(allow=('news\/',)), callback='parse_page', follow=True),
-        Rule(SgmlLinkExtractor(allow=('.html')), callback='parse', follow=True),
+        
+        Rule(SgmlLinkExtractor(allow=('\/(.+)\.html'), deny_extensions = ""), callback='parse_news'),
     )
-    def parse(self, response):
+
+
+    def parse_news(self, response):
         self.log('Hi, this is an item page! %s' % response.url)
-
-
-        item = YahooItem()
-        #item['id'] = sel.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
-        item['title'] = sel.xpath('//td[@id="item_name"]/text()').extract()
-        item['body'] = sel.xpath('//td[@id="item_description"]/text()').extract()
-
-
-        #'''
+        
         item = YahooItem()
         sel = Selector(response)
                 
@@ -38,4 +32,4 @@ class YahooSpider(CrawlSpider):
             item['title'] = ''
             item['body'] = ''
         yield item
-        #'''
+        
